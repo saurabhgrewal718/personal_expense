50b81da7bf6b3241a4c89029b165ff95049536ee
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-06-17 00:38:05 +0530

    the style of the expenses list have been finalised

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transactions.dart';
 
@@ -69,13 +70,16 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(border: Border.all(color: Colors.black,width: 2,)),
-                      child: Text(tx.amount.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.purple),),
+                      child: Text('\$ ${tx.amount}',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.purple),),
                     ),
                     Column(
                       crossAxisAlignment:CrossAxisAlignment.start ,
                       children:<Widget>[
                         Text(tx.title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17,color: Colors.black)),
-                        Text(tx.date.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15,color: Colors.black26)),
+                        Text(
+                          DateFormat('dd/MM/yyyy').format(tx.date),
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15,color: Colors.black26),
+                        ),
                       ],
                     ),
                   ],)
